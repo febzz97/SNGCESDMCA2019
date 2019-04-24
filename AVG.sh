f0d1bebbe5aa94d32fb617d754c25ed2b9dad681
febzz97 <48010429+febzz97@users.noreply.github.com>
2019-04-24 14:46:09 +0530

    Add files via upload

--- AVG.sh
+++ AVG.sh
@@ -8,3 +8,14 @@ n=`echo $((a+b+c+d+e))`
 echo $n
 avg=`expr $n/5`
 echo $avg
+
+
+
+echo enter a number
+read a
+fact=1
+for((i=1;i<=a;i++)) do
+fact=`echo $((fact*i))`
+echo the factorial of $a is $fact
+done
+
